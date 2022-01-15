d8497f9726c2a652a825daa5a259c26392959448
romeu3r <romeu3r@gmail.com>
2022-01-15 18:06:14 -0300

    Save, useNavigate

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,19 +1,8 @@
 import FormCard from 'components/FormCard';
 import { useParams } from 'react-router-dom';
-import { Movie } from 'types/movie';
 
-type Props = {
-    movie: Movie;
-}
 
 function Form() {
-    // const movie = {
-    //     id: 1,
-    //     image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    //     title: "The Witcher",
-    //     count: 2,
-    //     score: 4.5
-    // };
     const params = useParams();
 
     return (

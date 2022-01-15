409be2317df36a07dea682299a72b999a705f3d4
romeu3r <romeu3r@gmail.com>
2022-01-15 14:08:48 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,16 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    //Forma errada
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(resp => {
+        console.log(resp.data);
+    });
+
     return (
         <>
             <Pagination />
